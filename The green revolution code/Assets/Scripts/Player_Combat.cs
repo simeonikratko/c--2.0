356ca3dad0e1fe5f8ca6d7c6161ae3fd8d931244
Moiseicho <mateizahariev@gmail.com>
2021-03-14 07:53:42 +0200

    Adding finnal stuff

---
+++ The green revolution code/Assets/Scripts/Player_Combat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Combat : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+         //   void Attack();
+        }
+    }
+
+    //void Attack()
+    //{
+
+    //}
+}
